5ec6e64f443393c242b92f65470a5623e3ec7e03
Soton Ahmed <soton371@gmail.com>
2025-03-19 08:34:22 +0600

    contact di

--- lib/features/contacts/data/datasources/contact_local_data_source.dart
+++ lib/features/contacts/data/datasources/contact_local_data_source.dart
@@ -1,13 +1,10 @@
-
-import 'dart:convert';
-
 import '../../../../core/constants/constants.dart';
 import '../../../../core/static_data/simulate_api_client.dart';
 import '../models/contact_list_response_model.dart';
 
-class ContactLocalDataSources{
-  static Future<List<ContactListResponseModel>> fetchContactList()async{
+class ContactLocalDataSources {
+  static Future<List<ContactListResponseModel>> fetchContactList() async {
     final response = await SimulateApiClient.get(StaticDataPaths.contacts);
-    return contactListResponseModelFromJson(jsonEncode(response));
+    return contactListResponseModelFromJson(response);
   }
 }
--- lib/features/tickets/data/datasources/ticket_local_data_sources.dart
+++ lib/features/tickets/data/datasources/ticket_local_data_sources.dart
@@ -1,13 +1,11 @@
-import 'dart:convert';
-
 import 'package:tickets/core/constants/constants.dart';
 
 import '../../../../core/static_data/simulate_api_client.dart';
 import '../models/ticket_list_response_model.dart';
 
-class TicketLocalDataSources{
-  static Future<List<TicketListResponseModel>> fetchTicketList()async{
+class TicketLocalDataSources {
+  static Future<List<TicketListResponseModel>> fetchTicketList() async {
     final response = await SimulateApiClient.get(StaticDataPaths.tickets);
-    return ticketListResponseModelFromJson(jsonEncode(response));
+    return ticketListResponseModelFromJson(response);
   }
 }
