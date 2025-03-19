f103e6ac6eeabea98b990351739a5cf6bd595643
Soton Ahmed <soton371@gmail.com>
2025-03-19 20:51:52 +0600

    profile data

---
+++ lib/features/profile/domain/repositories/profile_repositories.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/errors/failures.dart';
+import '../../data/models/profile_response_model.dart';
+
+abstract class ProfileRepository {
+  Future<Either<Failure, List<ProfileResponseModel>>> fetchProfileResponse();
+}
---
+++ lib/features/profile/domain/usecases/profile_use_case.dart
@@ -0,0 +1,18 @@
+
+
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/errors/failures.dart';
+import '../../data/models/profile_response_model.dart';
+import '../repositories/profile_repositories.dart';
+
+class ProfileUseCase {
+  final ProfileRepository repository;
+
+  ProfileUseCase(this.repository);
+
+  Future<Either<Failure, List<ProfileResponseModel>>> call(){
+    return repository.fetchProfileResponse();
+  }
+}
+
